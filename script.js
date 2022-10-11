7a9de44bd2532dd6270abcdc5dee9747d6a6d75d
I-Maged <magedhanafy@gmail.com>
2022-10-11 21:06:16 +0200

    remove function

--- script.js
+++ script.js
@@ -1,30 +1,104 @@
 //Array saves all books
 const myLibrary = [];
 //Book object
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
 }
 //manually added a few books
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const theHobbit = new Book(
+  'The Hobbit',
+  'J.R.R. Tolkien',
+  295,
+  false,
+  0.9878796876546542321
+);
 const GameOfThrones = new Book(
   'A Game of Thrones',
   'George R. R. Martin',
   864,
-  true
+  true,
+  0.987987987543543543
 );
-const TheGuestList = new Book('The Guest List', 'Lucy Foley', 384, false);
+const TheGuestList = new Book(
+  'The Guest List',
+  'Lucy Foley',
+  384,
+  false,
+  0.542356432654765476
+);
 //pushing manually added books to the library
 myLibrary.push(theHobbit);
 myLibrary.push(GameOfThrones);
 myLibrary.push(TheGuestList);
-console.log(myLibrary);
+// console.log(myLibrary);
 
+//add new book
 document.querySelector('.form-btn').addEventListener('click', (e) => {
   e.preventDefault();
-  const bookTitle = document.querySelector('#title');
-  const bookRead = document.querySelector('#read');
-  console.log(bookRead.value);
+  const bookTitle = document.querySelector('#title').value;
+  const bookAuthor = document.querySelector('#author').value;
+  const bookPages = document.querySelector('#pages').value;
+  const bookRead =
+    document.querySelector('#read').value == 'yes' ? true : false;
+
+  const id = Math.random();
+  let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead, id);
+  myLibrary.push(newBook);
+  displayBook(newBook);
 });
+
+myLibrary.forEach((book) => displayBook(book));
+
+function displayBook(book) {
+  const cards = document.querySelector('.cards');
+
+  let bookCard = document.createElement('div');
+
+  bookCard.innerHTML = `<div class="book-card" id="div-01">
+  <h3 class="book-title">${book.title}</h3>
+  <div class="book-info">
+    <div class="info-field">
+      <p>Author: ${book.author}</p>
+    </div>
+    <div class="info-field">
+      <p># of pages: ${book.pages}</p>
+    </div>
+    <div class="info-field">
+      <p>Read status: ${book.read ? 'Read' : 'Not read yet'}</p>
+    </div>
+  </div>
+  <div class="book-btns">
+    <button class="edit-btn" value=${book.id}>Edit</button>
+    <button class="remove-btn" value=${book.id}>Remove</button>
+  </div>
+</div>`;
+
+  cards.appendChild(bookCard);
+
+  //remove buttons event listener
+  document.querySelectorAll('.remove-btn').forEach((removeBtn) => {
+    removeBtn.addEventListener('click', removeBook);
+  });
+}
+
+function removeBook(e) {
+  myLibrary.forEach(function (book, index, object) {
+    if (book.id == e.target.value) {
+      // remove book from UI
+      let bookBtns = e.target.parentNode;
+      let bookCard = bookBtns.parentNode;
+      let cards = bookCard.parentNode;
+
+      cards.style.display = 'none';
+      cards.remove();
+
+      //remove book from array
+      object.splice(index, 1);
+      console.log(myLibrary);
+    }
+  });
+}
